7f431bb04e9359c50ce9cab74e85de809c7f6e5e
krzysztofstefanski <krzysztof.stefanski82@gmail.com>
2019-04-11 09:52:32 +0200

    dodano hermetyzacje w  TypyScript

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from 'src/app/model/stock';
 
 @Component({
   selector: 'app-stock-item',
@@ -6,19 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-item.component.css']
 })
 export class StockItemComponent implements OnInit {
-  public name: string;
-  public code: string;
-  public price: number;
-  public previousPrice: number;
-  public positiveChange:boolean;
+  public stock : Stock
 
 
   constructor() { }
 
   ngOnInit() {
-    this.name = 'Test Stock Company';
-    this.code = 'TSC'; this.price = 85 ; this.previousPrice = 80;
-    this.positiveChange = this.price >= this.previousPrice;
+    this.stock = new Stock ('Test Stock Company','TSC',85,80)
+    
+  }
+
+  toggleFavourite (event){
+console.log('We are toggling the favourite state for this stock',event);
+this.stock.favorite = !this.stock.favorite;
   }
 
 }
