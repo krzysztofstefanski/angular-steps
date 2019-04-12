05332ede1faff615ef067150b7f3e2918bd83b35
krzysztofstefanski <krzysztof.stefanski82@gmail.com>
2019-04-12 12:59:02 +0200

    add NgClass

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Stock } from 'src/app/model/stock';
+import { Component, OnInit } from "@angular/core";
+import { Stock } from "src/app/model/stock";
 
 @Component({
-  selector: 'app-stock-item',
-  templateUrl: './stock-item.component.html',
-  styleUrls: ['./stock-item.component.css']
+  selector: "app-stock-item",
+  templateUrl: "./stock-item.component.html",
+  styleUrls: ["./stock-item.component.css"]
 })
 export class StockItemComponent implements OnInit {
-  public stock : Stock
+  public stock: Stock;
+  public stockClasses;
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.stock = new Stock ('Test Stock Company','TSC',85,80)
-    
+    this.stock = new Stock("Test Stock Company", "TSC", 85, 80);
+    let diff = (this.stock.price / this.stock.previousPrice)-1;
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+
+      "positive":this.stock.isPositiveChange(),
+      "negative":this.stock.isPositiveChange(),
+      "small-change":!largeChange,
+      "large-change":largeChange,
+    }
   }
 
-  toggleFavourite (event){
-console.log('We are toggling the favourite state for this stock',event);
-this.stock.favorite = !this.stock.favorite;
+  toggleFavourite(event) {
+    console.log("We are toggling the favourite state for this stock", event);
+    this.stock.favorite = !this.stock.favorite;
   }
-
 }
